454d8566b7998b3647c6c2fdf72a2d887eb23d33
Kol Crooks <kolcrooks10@gmail.com>
2019-05-22 20:57:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,4 +26,4 @@ app.get('/post',(req,res)=>{
 
 })
 
-app.listen(3000, console.log("Server running on port 3000!"));
+app.listen(process.env.PORT || 3000, console.log("Server running on port 3000!"));
