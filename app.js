52129eb154dbfc4d5e091b5ecea54c1fc434b583
Kol Crooks <kolcrooks10@gmail.com>
2019-05-28 10:00:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.get('/get',(req,res)=>{
     HS.findOrCreate(
         {where:{id:0}}
     ).then((val,created)=>{
-        console.log(val.dataValues)
-        res.send(val.dataValues.score + "")
+        console.log(val[0].dataValues)
+        res.send(val[0].dataValues.score + "")
     })
 
 });
