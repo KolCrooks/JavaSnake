250367851629b8ade76603f115d0cb62aa5acae4
Kol Crooks <kolcrooks10@gmail.com>
2019-10-17 08:53:37 -0400

    comments

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+//Lib Stuff
 let express = require('express');
 let fs = require('fs');
 let Sequelize = require('sequelize')
 let app = express();
+
+//Create the model for the database
 const Model = Sequelize.Model;
 class HS extends Model {}
+
+//Setup database
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: './database.sqlite',
     logging: false
 });
 
-
+//Define the model
 HS.init({
   // attributes
   id: {
@@ -26,24 +31,33 @@ HS.init({
 }, {
   sequelize,
   modelName: 'hs'
-  // options
 });
+
+//Set route for getting highscore
 app.get('/get',(req,res)=>{
+
+  //get highscore
     HS.findOrCreate(
         {where:{id:0}}
     ).then((val,created)=>{
         console.log(val[0].dataValues)
+        //Send response
         res.send(val[0].dataValues.score + "")
     })
 
 });
 
+//Set route for setting new highscore
 app.get('/post',(req,res)=>{
+
+  //Sets highscore
     HS.update({score:req.headers.score}, {where:{id:0}}).then(()=>{
         res.send("success");
     })
 });
 
+//Sync Database
 sequelize.sync();
 
+//Setup server
 app.listen(process.env.PORT || 3000, console.log("Server running on port 3000!"));
